579c00aeefbee6a8d96c71dc6c7d27fbb6e00978
Urez <paezcuf@gmail.com>
2022-06-22 01:18:07 -0500

    creacion del proyecto

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Events\WebsocketDemoEvent;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatsController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+    broadcast(new WebsocketDemoEvent('some data'));
+
+    return view('welcome');
+});
+
+
+Route::get('/chats', [ChatsController::class, 'index']); 
+
+Route::get('/messages',[ChatsController::class, 'fetchMessages']);
+Route::post('/messages',[ChatsController::class, 'sendMessage']);
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
